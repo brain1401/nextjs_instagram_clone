f7cfccf6d4bbb4600231d1cb4ee420944e82afbc
Aiden Hong <brain1401@gmail.com>
2023-06-22 15:58:38 +0900

    modal 포탈 설정

---
+++ src/app/components/ui/ModalPortal.tsx
@@ -0,0 +1,14 @@
+import reactDom from 'react-dom';
+
+type Props = {
+  children : React.ReactNode;
+}
+
+export default function ModalPortal ({children}: Props){ //children에 전달한 요소를 portal이라는 id를 가진 요소에 연결해주는 역할이다.
+  if(typeof window === 'undefined') { //서버사이드 환경일때는 아무것도 반환해주지 않을 것이라는것을 의미한다.
+    return null;
+  }
+
+  const node = document.getElementById('portal') as Element;
+  return reactDom.createPortal(children, node)
+}
