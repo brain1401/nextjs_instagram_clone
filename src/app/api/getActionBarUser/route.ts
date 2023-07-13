42921e0e2894c63c7bfe2b91ca1929264300c028
Aiden Hong <brain1401@gmail.com>
2023-07-14 05:46:36 +0900

    에러메세지 오타 수정

--- src/app/api/getActionBarUser/route.ts
+++ src/app/api/getActionBarUser/route.ts
@@ -6,7 +6,7 @@ import { authOptions } from "../auth/[...nextauth]/route";
 export async function GET(_:NextRequest) {
   const session = await getServerSession(authOptions);
   if(!session?.user?.email){
-    throw new Error('새션 정보가 없숩니다!');
+    throw new Error('세션 정보가 없숩니다!');
   }
   const email = session.user.email;
   return NextResponse.json(await getActionBarUserByEmail(email));
