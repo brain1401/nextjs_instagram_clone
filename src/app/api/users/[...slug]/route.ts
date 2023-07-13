00fa0bb2de9580de6dbf209506a0c3b1d4b59d99
Aiden Hong <brain1401@gmail.com>
2023-07-14 06:42:38 +0900

    pretify

--- src/app/api/users/[...slug]/route.ts
+++ src/app/api/users/[...slug]/route.ts
@@ -1,31 +1,32 @@
-import { getLikedPostByDisplayname, getPostByDisplayname, getSavedPostByDisplayname } from "@/service/posts";
+import {
+  getLikedPostByDisplayname,
+  getPostByDisplayname,
+  getSavedPostByDisplayname,
+} from "@/service/posts";
 import { NextRequest, NextResponse } from "next/server";
-
 
 type Context = {
   params: {
     slug: string[]; //slug/slug/slug
-  }
-}
+  };
+};
 export async function GET(_: NextRequest, context: Context) {
-  const {slug} = context.params;
+  const { slug } = context.params;
 
-  if(!slug || !Array.isArray(slug) || slug.length < 2) {
-    return new NextResponse('Bad Request', {status: 400})
+  if (!slug || !Array.isArray(slug) || slug.length < 2) {
+    return new NextResponse("Bad Request", { status: 400 });
   }
 
   const [displayname, query] = slug;
 
   let request = getPostByDisplayname;
-  if(query === 'saved'){
+  if (query === "saved") {
     request = getSavedPostByDisplayname;
-
-  }else if(query === 'liked'){
+  } else if (query === "liked") {
     request = getLikedPostByDisplayname;
   }
-  
+
   const result = await request(displayname);
 
-  return NextResponse.json(result)
-
+  return NextResponse.json(result);
 }
