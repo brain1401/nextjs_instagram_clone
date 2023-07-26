2fe39f89ce008bae3fe46ce9b68c5a9a6af8d996
Aiden Hong <brain1401@gmail.com>
2023-07-27 06:24:20 +0900

    navbar 모바일 UI 개선

--- src/app/components/Navber.tsx
+++ src/app/components/Navber.tsx
@@ -8,6 +8,9 @@ import ColorButton from "./ui/ColorButton";
 import { useSession, signIn, signOut } from "next-auth/react";
 import Avatar from "./Avatar";
 import useMe from "@/hooks/me";
+import { useMediaQuery } from "react-responsive";
+import { useState, useEffect } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
 
 const ICON_STYLE = "text-3xl hover:cursor-pointer";
 
@@ -33,8 +36,18 @@ export default function Navbar() {
   const pathname = usePathname();
   const { data: session } = useSession();
   const { user, isLoading: loading, error } = useMe();
+  const [mobile, setMobile] = useState(false);
+  const [isMenuOpened, setIsMenuOpened] = useState(false);
 
-  return (
+  const isMobile = useMediaQuery({
+    query: "(max-width:767px)",
+  });
+
+  useEffect(() => {
+    setMobile(isMobile);
+  }, [isMobile]);
+
+  return !mobile ? ( //PC
     <nav className="flex items-center justify-between h-12 px-5">
       <Link href="/">
         <h1 className="text-2xl font-bold md:text-3xl md:px-10">Instagram</h1>
@@ -70,6 +83,64 @@ export default function Navbar() {
               className="w-[3.6rem]"
             />
           )}
+        </li>
+      </ul>
+    </nav>
+  ) : (
+    //Mobile
+    <nav className="flex justify-between h-12 px-5">
+      <Link href="/" className="self-center">
+        <h1 className="text-2xl font-bold md:text-3xl md:px-10">Instagram</h1>
+      </Link>
+
+      <button onClick={() => setIsMenuOpened(!isMenuOpened)}>
+        <AiOutlineMenu size={30} className="absolute top-0 mt-3 right-3" />
+      </button>
+      <ul className="flex flex-col gap-[0.3rem] mt-14 items-end">
+        {isMenuOpened &&
+          menu.map((item) => (
+            <li key={item.href}>
+              <div className="flex justify-center items-center bg-black text-white rounded-md w-[3.6rem] h-[3rem]">
+                <Link href={item.href} onClick={() => setIsMenuOpened(false)}>
+                  {pathname === item.href ? item.clickedIcon : item.icon}
+                </Link>
+              </div>
+            </li>
+          ))}
+
+        {isMenuOpened && user && (
+          <li>
+            <div className="flex justify-center items-center rounded-md bg-black text-white w-[3.6rem] h-[3rem]">
+              <Link
+                onClick={() => setIsMenuOpened(false)}
+                href={`/user/${user.displayname}`}
+              >
+                <Avatar image={user.userimage} size="small" highlight={true} />
+              </Link>
+            </div>
+          </li>
+        )}
+        <li>
+          {isMenuOpened &&
+            (session ? (
+              <ColorButton
+                onClick={() => {
+                  signOut();
+                  setIsMenuOpened(false);
+                }}
+                text="로그아웃"
+                className="w-[3.6rem]"
+              />
+            ) : (
+              <ColorButton
+                onClick={() => {
+                  signIn();
+                  setIsMenuOpened(false);
+                }}
+                text="로그인"
+                className="w-[3.6rem]"
+              />
+            ))}
         </li>
       </ul>
     </nav>
